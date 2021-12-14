19746238be04103d81079bb97cb20cbac2a1b0f9
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2021-12-14 19:38:41 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const sleep = require('../functions/sleep');
+const sendToPebmedApi = require('../services/send-to-pebmed-api');
+const instantiveRabbiMq = require('../functions/instantiveRabbiMq');
+
+class Automation {
+  async run(ctx) {
+    try {
+      const rabbitMq = await instantiveRabbiMq(ctx.credentials);
+
+      const numberOfMessages = await rabbitMq.getMessageCount(ctx.credentials);
+
+      rabbitMq.channel.consume(
+        ctx.credentials.queueName,
+        async (msg) => {
+          if (msg !== null) {
+            const messageFromQueue = JSON.parse(msg.content.toString());
+            
+            try {
+              const response = await sendToApi(messageFromQueue);
+              console.log(response);
+            } catch (error) {
+                console.log(error);
+              });
+            }
+
+            await sleep(5000);
+          }
+        },
+        { noAck: true }
+      );
+
+      setTimeout(async () => {
+        await rabbitMq.closeChannel();
+        await rabbitMq.closeConnection();
+      }, numberOfMessages * 15000);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+}
+
+module.exports = new Automation();
+// const test = new Automation();
+// test.run({
+//   credentials: {
+//     amqpUrl:
+//       'amqps://...',
+//     queueName: 'test-queue',
+//   },
+// });
