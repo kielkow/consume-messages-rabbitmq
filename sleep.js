dfc163abd5201b7a371783b41054c74210a3f2da
Matheus Kielkowski <matheuskiel@fiorifer.com.br>
2021-12-14 19:40:01 -0300

    Create sleep.js

---
+++ sleep.js
@@ -0,0 +1,9 @@
+module.exports = async (milliseconds) => {
+  const date = Date.now();
+
+  let currentDate = null;
+
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+};
